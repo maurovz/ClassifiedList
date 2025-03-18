cbd767c1b7048b96ecb4b4b6d146ebf666de4830
Mauricio Vazquez <maurovz@gmail.com>
2025-03-18 12:31:20 +0100

    Initial Commit

---
+++ ClassifiedList/ClassifiedListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassifiedListApp.swift
+//  ClassifiedList
+//
+//  Created by Mauricio Vazquez on 18/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ClassifiedListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClassifiedList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClassifiedList
+//
+//  Created by Mauricio Vazquez on 18/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
