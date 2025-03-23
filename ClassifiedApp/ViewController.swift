482f0b7d3132d3afe050cf2c738fdc163821b1ae
Mauricio Vazquez <maurovz@gmail.com>
2025-03-23 16:51:40 +0100

    Add main iOS app target

---
+++ ClassifiedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassifiedApp
+//
+//  Created by Mauricio Vazquez on 23/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
