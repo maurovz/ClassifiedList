2003e813fc37ad7488deea46aad8b6e9524d964d
Mauricio Vazquez <maurovz@gmail.com>
2025-03-23 17:06:00 +0100

    Add factory methods

--- ClassifiedApp/SceneDelegate.swift
+++ ClassifiedApp/SceneDelegate.swift
@@ -6,8 +6,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let viewController = ViewController()
-        window?.rootViewController = viewController
+        
+        let factory = ServiceFactory.shared
+        let repository = factory.createClassifiedRepository()
+        let imageLoader = factory.createImageLoader()
+        
+        let viewModel = ClassifiedListViewModel(repository: repository)
+        let viewController = ClassifiedListViewController(viewModel: viewModel, imageLoader: imageLoader)
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 }
